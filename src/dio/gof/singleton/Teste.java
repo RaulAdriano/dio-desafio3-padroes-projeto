5644031a9e988c49c912c5f8071a4017b58d4e28
Raul Adriano Ramos <raul.adriano.ramos@gmail.com>
2022-08-09 18:22:57 -0300

    Initial commit

---
+++ src/dio/gof/singleton/Teste.java
@@ -0,0 +1,22 @@
+package dio.gof.singleton;
+
+public class Teste {
+    public static void main(String[] args) {
+        //testes relacionados ao padrao de projeto Singleton
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+    }
+}
